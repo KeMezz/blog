f1f278255b7ae80529b2a3c2e3f21909346b5d42
정형진 <88423849+KeMezz@users.noreply.github.com>
2022-12-31 16:46:05 +0900

    create basic layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,8 @@
+import { Link } from "gatsby";
 import React, { ReactElement } from "react";
+import { FaTwitter } from "@react-icons/all-files/fa/FaTwitter";
+import { FaGithub } from "@react-icons/all-files/fa/FaGithub";
+import { FaInstagram } from "@react-icons/all-files/fa/FaInstagram";
 
 interface LayoutProps {
   children: ReactElement | ReactElement[];
@@ -7,9 +11,34 @@ interface LayoutProps {
 function Layout({ children }: LayoutProps) {
   return (
     <main>
-      <header></header>
-      {children}
-      <footer></footer>
+      <header className="w-full h-20 bg-white border-b">
+        <div className="h-full flex items-center justify-between max-w-7xl px-6 mx-auto">
+          <h1 className="text-3xl font-bold">.storage</h1>
+          <nav className="flex gap-8">
+            <Link to="/">블로그</Link>
+            <Link to="/">방명록</Link>
+            <Link to="/">포트폴리오</Link>
+          </nav>
+        </div>
+      </header>
+      <section className="bg-gray-100">
+        <div className="max-w-7xl mx-auto px-6 ">{children}</div>
+      </section>
+      <footer className="p-12">
+        <div className="max-w-7xl mx-auto px-6 flex flex-col gap-6">
+          <h2 className="text-3xl font-bold">Hyeongjin Developer Blog</h2>
+          <p className="text-sm">
+            오히려 잘 모르기 때문에 글로 남기려고 합니다.
+            <br />
+            함께 채워가는 블로그를 만들고 싶어요!
+          </p>
+          <div className="flex gap-6 text-3xl text-gray-600">
+            <FaGithub />
+            <FaInstagram />
+            <FaTwitter />
+          </div>
+        </div>
+      </footer>
     </main>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,9 @@ import Seo from "../components/Seo";
 const IndexPage = () => {
   return (
     <Layout>
-      <main className="bg-slate-600 py-10">
-        <h1>hello</h1>
-      </main>
+      <div className="flex justify-center items-center w-full h-56 font-bold text-4xl">
+        Hello, world!
+      </div>
     </Layout>
   );
 };
