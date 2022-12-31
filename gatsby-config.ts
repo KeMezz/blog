2568cf5c3f8f9d8a4f6c44a9bed01952eb05cd46
정형진 <88423849+KeMezz@users.noreply.github.com>
2022-12-31 16:45:52 +0900

    add support for google fonts
    gatsby-omni-font-loader를 통해 구글 폰트를 불러와서 쓸 수 있도록 했습니다.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,13 +4,31 @@ const config: GatsbyConfig = {
   siteMetadata: {
     title: `dotStorage`,
     siteUrl: `https://www.yourdomain.tld`,
-    description: `소소한 저의 일상과 개발에 대한 내용을 담는 블로그에요.`,
+    description: `소소한 일상과 개발에 대한 내용을 담는 블로그에요.`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [`gatsby-plugin-postcss`],
+  plugins: [
+    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [
+          `https://fonts.googleapis.com`,
+          `https://fonts.gstatic.com`,
+        ],
+        web: [
+          {
+            name: `Noto Sans Korean`,
+            file: `https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap`,
+          },
+        ],
+      },
+    },
+  ],
 };
 
 export default config;
