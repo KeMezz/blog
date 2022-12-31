38f2ad8e80edf93f20a77fe76d1cca0adf43d875
정형진 <88423849+KeMezz@users.noreply.github.com>
2022-12-31 18:41:37 +0900

    add dark mode for layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,22 +11,22 @@ interface LayoutProps {
 function Layout({ children }: LayoutProps) {
   return (
     <main>
-      <header className="w-full h-30 lg:h-20 bg-white border-b">
+      <header className="w-full h-30 lg:h-20 bg-white dark:bg-zinc-800 dark:text-white border-b dark:border-gray-600">
         <div className="flex flex-col h-full lg:flex-row lg:px-8 lg:mx-auto lg:max-w-7xl">
-          <div className="w-full h-20 lg:h-auto flex justify-center lg:justify-between items-center border-b lg:border-none">
+          <div className="w-full h-20 lg:h-auto flex justify-center lg:justify-between items-center border-b dark:border-gray-600 lg:border-none">
             <h1 className="text-4xl font-extrabold mt-2">.cache</h1>
           </div>
-          <nav className="w-full lg:w-auto h-10 lg:h-auto grid grid-cols-3 place-items-center divide-x lg:divide-none">
+          <nav className="w-full lg:w-auto h-10 lg:h-auto grid grid-cols-3 place-items-center divide-x lg:divide-none dark:divide-gray-600">
             <HeaderLink to="/" title="블로그" />
             <HeaderLink to="/" title="방명록" />
             <HeaderLink to="/" title="포트폴리오" />
           </nav>
         </div>
       </header>
-      <section className="bg-gray-100">
+      <section className="bg-gray-100 dark:bg-zinc-700 dark:text-white">
         <div className="max-w-7xl mx-auto px-6 ">{children}</div>
       </section>
-      <footer className="border-t py-8">
+      <footer className="border-t dark:border-gray-600 py-8 bg-white dark:bg-zinc-800 dark:text-white">
         <div className="flex flex-col items-center lg:items-start text-center lg:text-left lg:max-w-7xl lg:mx-auto lg:px-8 gap-4 lg:gap-6">
           <h2 className="text-2xl font-bold lg:text-3xl">
             Hyeongjin Developer Blog
@@ -36,7 +36,7 @@ function Layout({ children }: LayoutProps) {
             <br />
             함께 채워가는 블로그를 만들고 싶어요!
           </p>
-          <div className="flex gap-6 text-2xl text-gray-600">
+          <div className="flex gap-6 text-2xl text-gray-600 dark:text-gray-300">
             <FaGithub />
             <FaInstagram />
             <FaTwitter />
