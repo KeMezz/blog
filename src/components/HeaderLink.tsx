d4804c53d77bc192ab2deed96c9d9e3a2c7ea38b
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-01-01 18:59:03 +0900

    edit header styles

--- src/components/HeaderLink.tsx
+++ src/components/HeaderLink.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { Link } from "gatsby";
+import { cls } from "../utils/cls";
 
 interface HeaderLinkProps {
   to: string;
   title: string;
+  active: boolean;
 }
 
-const HeaderLink = ({ to, title }: HeaderLinkProps) => {
+const HeaderLink = ({ to, title, active }: HeaderLinkProps) => {
   return (
     <Link
       to={to}
-      className="text-sm w-full h-full flex justify-center items-center lg:w-28"
+      className={cls(
+        "text-sm w-full h-full flex justify-center items-center lg:w-28",
+        active
+          ? "bg-white dark:bg-zinc-900 lg:bg-transparent lg:text-cyan-500 font-semibold shadow-sm lg:shadow-none"
+          : "bg-gray-200 dark:bg-zinc-700 lg:bg-transparent shadow-inner lg:shadow-none"
+      )}
     >
       {title}
     </Link>
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,32 +3,44 @@ import { FaTwitter } from "@react-icons/all-files/fa/FaTwitter";
 import { FaGithub } from "@react-icons/all-files/fa/FaGithub";
 import { FaInstagram } from "@react-icons/all-files/fa/FaInstagram";
 import HeaderLink from "./HeaderLink";
+import { Link } from "gatsby";
 
 interface LayoutProps {
   children: ReactElement | ReactElement[];
 }
 
 function Layout({ children }: LayoutProps) {
+  const pathname = window.location.pathname;
   return (
     <main>
-      <header className="w-full h-30 lg:h-20 bg-white dark:bg-zinc-900 dark:text-white border-b-2 dark:border-gray-600 shadow-inner">
+      <header className="w-full h-30 lg:h-20 bg-white dark:bg-zinc-900 dark:text-white">
         <div className="flex flex-col h-full lg:flex-row lg:px-8 lg:mx-auto lg:max-w-7xl">
-          <div className="w-full h-20 lg:h-auto flex justify-center lg:justify-between items-center border-b-2 dark:border-gray-600 lg:border-none">
-            <h1 className="text-4xl font-extrabold font-['Pacifico']">
-              .cache
-            </h1>
+          <div className="w-full h-20 lg:h-auto flex justify-center lg:justify-between items-center lg:border-none">
+            <Link to="/">
+              <h1 className="text-4xl font-extrabold font-['Pacifico']">
+                .cache
+              </h1>
+            </Link>
           </div>
-          <nav className="w-full lg:w-auto h-10 lg:h-auto grid grid-cols-3 place-items-center divide-x-2 lg:divide-none dark:divide-gray-600">
-            <HeaderLink to="/" title="블로그" />
-            <HeaderLink to="/" title="방명록" />
-            <HeaderLink to="/" title="검색" />
+          <nav className="w-full lg:w-auto h-10 lg:h-auto grid grid-cols-3 place-items-center divide-x divide-gray-300 dark:divide-zinc-900 lg:divide-none">
+            <HeaderLink to="/" title="홈" active={pathname === "/"} />
+            <HeaderLink
+              to="/blog"
+              title="블로그"
+              active={pathname.startsWith("/blog")}
+            />
+            <HeaderLink
+              to="/search"
+              title="검색"
+              active={pathname.startsWith("/search")}
+            />
           </nav>
         </div>
       </header>
       <section className="bg-gray-100 dark:bg-zinc-800 dark:text-white shadow-inner">
         <div className="max-w-7xl mx-auto px-6">{children}</div>
       </section>
-      <footer className="border-t-2 dark:border-gray-600 py-8 bg-white dark:bg-zinc-900 dark:text-white shadow-inner">
+      <footer className="border-t-2 dark:border-gray-600 py-8 bg-white dark:bg-zinc-900 dark:text-white">
         <div className="flex flex-col items-center lg:items-start text-center lg:text-left lg:max-w-7xl lg:mx-auto lg:px-8 gap-4 lg:gap-6">
           <h2 className="text-2xl font-bold lg:text-3xl font-['Pacifico']">
             Hyeongjin Developer Blog
