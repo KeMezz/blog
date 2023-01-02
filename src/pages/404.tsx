2077860897b87cfd7c7c69a1745cd131760bd746
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-01-02 22:38:05 +0900

    Update 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,49 +1,29 @@
-import * as React from "react"
-import { Link, HeadFC, PageProps } from "gatsby"
-
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+import * as React from "react";
+import { Link, HeadFC, PageProps } from "gatsby";
+import Layout from "../components/Layout";
 
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
+    <Layout hideNav>
+      <div className="flex flex-col justify-center items-center py-32">
+        <h1 className="text-center font-bold text-2xl mb-8">
+          ⛔️ 404 Not Found ️⛔️
+        </h1>
+        <p className="text-center mb-6">
+          죄송해요..😔
+          <br />
+          요청하신 페이지는 없는 페이지에요.
+        </p>
+        <Link to="/" className="pt-6 border-t mb-16 text-sm">
+          <button className="px-4 py-2 bg-cyan-500 text-white rounded-xl">
+            홈으로 돌아가기
+          </button>
+        </Link>
+      </div>
+    </Layout>
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
 
-export const Head: HeadFC = () => <title>Not found</title>
+export const Head: HeadFC = () => <title>Not found</title>;
