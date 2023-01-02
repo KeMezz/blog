cf7ef90d76dc6ecb913fef7cf1578ce6e8878888
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-01-02 22:37:30 +0900

    mdx custom styling w/ tailwind typo

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,6 +1,7 @@
 import { graphql } from "gatsby";
 import React, { ReactElement } from "react";
 import Layout from "../../components/Layout";
+import { FaRegCalendar } from "@react-icons/all-files/fa/FaRegCalendar";
 
 interface PostDetailProps {
   data: Queries.PostDetailQuery;
@@ -8,8 +9,42 @@ interface PostDetailProps {
 }
 
 const PostDetail = ({ data, children }: PostDetailProps) => {
-  console.log(data);
-  return <Layout>{children}</Layout>;
+  const title = data.mdx?.frontmatter?.title;
+  return (
+    <Layout disablePaddings>
+      <section className="pb-14">
+        <div className="flex flex-col gap-6 text-2xl text-center px-4 py-16">
+          <span className="text-5xl">{data.mdx?.frontmatter?.emoji}</span>
+          <div className="flex flex-wrap justify-center items-center gap-2 px-4">
+            {data.mdx?.frontmatter?.tags?.map((tag, i) => (
+              <p
+                className="text-xs px-3 py-1 bg-white dark:bg-zinc-900 rounded-full"
+                key={i}
+              >
+                #{tag}
+              </p>
+            ))}
+          </div>
+          {/* <br /> */}
+          {/* <h1 className="leading-snug">{title}</h1> */}
+        </div>
+        <article className="bg-white dark:bg-zinc-900 p-6 py-10 lg:p-12 xl:rounded-xl">
+          <div className="mb-6">
+            <h1 className="border-b-2 pb-4 text-2xl font-semibold mb-4">
+              {title}
+            </h1>
+            <div className="flex items-center gap-2 text-gray-500">
+              <FaRegCalendar className="text-sm" />
+              <p className="text-xs">{data.mdx?.frontmatter?.date}</p>
+            </div>
+          </div>
+          <div className="prose prose-sm lg:prose-base prose-h1:text-3xl prose-h1:pt-8 prose-h1:mb-0 prose-h2:mt-10 max-w-none dark:prose-invert prose-headings:before:text-sm lg:prose-headings:before:text-lg prose-headings:before:text-zinc-200 dark:prose-headings:before:text-zinc-600 prose-headings:before:mr-2 prose-h1:before:content-['#'] prose-h2:before:content-['##'] prose-h3:before:content-['###'] prose-h4:before:content-['####'] prose-h5:before:content-['#####'] prose-h6:before:content-['######']">
+            {children}
+          </div>
+        </article>
+      </section>
+    </Layout>
+  );
 };
 
 export const query = graphql`
@@ -20,8 +55,8 @@ export const query = graphql`
         emoji
         slug
         title
+        tags
       }
-      excerpt
     }
   }
 `;
