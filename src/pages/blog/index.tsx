4bbf762e931f2901ba8527159b3aac7e6c39f626
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-01-25 00:22:37 +0900

    최근에 올라온 포스트 제외 disabled 처리

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -4,14 +4,18 @@ import Layout from "../../components/Layout";
 import PostPreview from "../../components/PostPreview";
 import Seo from "../../components/Seo";
 
-const Blog = ({ data }: PageProps<Queries.BlogPostsQuery>) => {
+const Posts = ({ data }: PageProps<Queries.RecentPostsQuery>) => {
   return (
     <Layout>
       <div className="pt-6 pb-8">
         <select className="border-none bg-transparent font-semibold text-lg lg:text-xl focus:ring-transparent mb-2">
-          <option>최근에 올라온 포스트</option>
-          <option>태그별로 보기</option>
-          <option>시리즈 모아보기</option>
+          <option value="recent">최근에 올라온 포스트</option>
+          <option value="tag" disabled>
+            태그별로 보기
+          </option>
+          <option value="series" disabled>
+            시리즈 모아보기
+          </option>
         </select>
         <section className="grid grid-flow-dense justify-items-stretch grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-6">
           {data.allMdx.nodes.map((mdx) => (
@@ -31,7 +35,7 @@ const Blog = ({ data }: PageProps<Queries.BlogPostsQuery>) => {
 };
 
 export const query = graphql`
-  query BlogPosts {
+  query RecentPosts {
     allMdx(sort: { frontmatter: { date: DESC } }) {
       nodes {
         frontmatter {
@@ -48,4 +52,4 @@ export const query = graphql`
 
 export const Head = () => <Seo title="블로그" />;
 
-export default Blog;
+export default Posts;
