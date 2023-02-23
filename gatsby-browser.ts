0743bf60c5d6829453ff403b45e3236f75773ed6
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-02-23 20:48:17 +0900

    사용하지 않는 페이지 전부 삭제

---
+++ gatsby-browser.ts
@@ -0,0 +1,4 @@
+import "./src/styles/global.css";
+import "prismjs/themes/prism-tomorrow.css";
+import "prismjs/plugins/line-numbers/prism-line-numbers.css";
+import "prismjs/plugins/command-line/prism-command-line.css";
