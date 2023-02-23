bb723ecbdfa20c7fd603d5dd660ec987c12a98d6
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-02-23 16:44:55 +0900

    custom adaptor로 useQueryParam v2 활성화
    https://github.com/alexluong/gatsby-packages/issues/46

--- gatsby-browser.ts
+++
@@ -1,4 +0,0 @@
-import "./src/styles/global.css";
-import "prismjs/themes/prism-tomorrow.css";
-import "prismjs/plugins/line-numbers/prism-line-numbers.css";
-import "prismjs/plugins/command-line/prism-command-line.css";
