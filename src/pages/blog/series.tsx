25622c18b7f42e4bd20e2a5e20ecb600688aab47
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-02-23 16:08:49 +0900

    시리즈 페이지 임시 작성

---
+++ src/pages/blog/series.tsx
@@ -0,0 +1,25 @@
+import { graphql } from "gatsby";
+import * as React from "react";
+import BlogSelector from "../../components/blog-selector";
+import Layout from "../../components/layout";
+import PostPreview from "../../components/post-preview";
+import Seo from "../../components/seo";
+
+interface PostsProps {
+  data: Queries.RecentPostsQuery;
+}
+
+const Series = () => {
+  return (
+    <Layout>
+      <div className="pt-6 pb-8">
+        <BlogSelector value="series" />
+        <section className="grid grid-flow-dense justify-items-stretch grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-6"></section>
+      </div>
+    </Layout>
+  );
+};
+
+export const Head = () => <Seo title="블로그" />;
+
+export default Series;
