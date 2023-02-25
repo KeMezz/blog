721936001e7bdc588569b45007aa3fec692a8d71
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-02-25 22:15:06 +0900

    build시 헤더 색상이 의도치 않게 표시되는 문제 수정

--- src/components/header-link.tsx
+++ src/components/header-link.tsx
@@ -10,13 +10,26 @@ interface HeaderLinkProps {
 
 const HeaderLink = ({ to, title, partiallyActive }: HeaderLinkProps) => {
   const isDark = useMediaQuery({ query: "(prefers-color-scheme: dark)" });
+  const isMobile = useMediaQuery({ query: "(max-width: 1025px)" });
+  const [activeBgColor, setActiveBgColor] = useState("");
+
+  useEffect(() => {
+    if ((!isMobile && isDark) || (isDark && isMobile)) {
+      console.log("1");
+      setActiveBgColor("#18181b");
+    } else if ((!isDark && !isMobile) || (!isDark && isMobile)) {
+      console.log("2");
+      setActiveBgColor("#ffffff");
+    }
+  }, [isDark, isMobile]);
+
   return (
     <Link
       to={to}
       className="text-sm w-full h-full flex justify-center items-center lg:w-28 bg-gray-200 dark:bg-zinc-700 lg:bg-transparent shadow-inner lg:shadow-none"
       activeClassName="lg:bg-transparent lg:text-cyan-500 font-semibold shadow-none shadow-sm lg:shadow-none"
       partiallyActive={partiallyActive}
-      activeStyle={{ backgroundColor: isDark ? "#18181b" : "#ffffff" }}
+      activeStyle={{ backgroundColor: activeBgColor }}
     >
       {title}
     </Link>
